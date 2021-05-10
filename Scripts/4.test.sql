fb15897d648641e8ee6d30395360abe0f0113599
ShinBG <shinws9090@naver.com>
2021-05-10 16:45:19 +0900

    마이바티스 데이터배이스 연결완료

---
+++ Scripts/4.test.sql
@@ -0,0 +1,5 @@
+select * from addresses;
+select * from course_enrollment;
+select * from courses;
+select * from students;
+select * from tutors;
---
+++ src/test/java/spring5_mybatis_study/config/ContestDataSourceTest.java
@@ -0,0 +1,40 @@
+package spring5_mybatis_study.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ContestDataSource.class})
+public class ContestDataSourceTest {
+	
+	private static final Log log = LogFactory.getLog(ContestDataSourceTest.class);
+	
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void testDataSource() throws SQLException {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		log.debug("DataSource " + dataSource);
+		log.debug("LoginTimeout " + dataSource.getLoginTimeout());
+		Assert.assertNotNull(dataSource);
+
+	}
+
+}
